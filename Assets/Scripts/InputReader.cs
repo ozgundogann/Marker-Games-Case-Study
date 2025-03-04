7d78f6b34a535481c93981d634a20e5b9ffe066c
ozgunndogan <ozgundogan2000@gmail.com>
2025-03-04 19:18:51 +0300

    Add input reader

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class InputReader : MonoBehaviour
+    {
+        public static event Action OnSpacePressed;
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                OnSpacePressed?.Invoke();
+            }
+        }
+    }
+}
