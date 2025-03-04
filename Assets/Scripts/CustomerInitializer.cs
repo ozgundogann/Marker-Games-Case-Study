14bf8ff940d41f3692a73eee1d8490911ef4ce47
ozgunndogan <ozgundogan2000@gmail.com>
2025-03-04 19:18:41 +0300

    Add customer initializer

---
+++ Assets/Scripts/CustomerInitializer.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CustomerInitializer : MonoBehaviour
+{
+    [SerializeField] private Customer prefab;
+    [SerializeField] private int objectCount = 30; 
+    [SerializeField] private float radius = 10f; 
+
+    public static readonly int MaxCustomerCount = 10;
+    void Start()
+    {
+        SpawnObjects();
+    }
+
+    void SpawnObjects()
+    {
+        for (int i = 0; i < MaxCustomerCount; i++)
+        {
+            float angle = Random.Range(0f, 360f);
+            float radian = angle * Mathf.Deg2Rad;
+            
+            float randomRadius = Mathf.Sqrt(Random.Range(0f, 1f)) * radius;
+
+            Vector3 position = new Vector3(
+                Mathf.Cos(radian) * randomRadius, 
+                0f,
+                Mathf.Sin(radian) * randomRadius
+            );
+
+            Customer newCustomer = Instantiate(prefab, transform);
+            newCustomer.transform.position = position;
+        }
+    }
+}
